6dbb1ed6aacc5716d7bf863f458b6b9657377815
Yaroslav <dombrovskiy91@gmail.com>
2020-10-17 19:54:10 +0300

    Redux Store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
+import {createStore} from "redux";
+
 
 const reducer = (state = 0, action) => {
-
     switch (action.type) {
         case 'INC':
             return state + 1;
-
         case 'DEC':
             return state - 1;
 
-        default: return state;
+        default:
+            return state;
     }
+    return 0;
 };
-let state = reducer(undefined, {});
-    state = reducer(state, {type: 'INC'});
-    state = reducer(state, {type: 'INC'});
-    state = reducer(state, {type: 'DEC'});
-
-console.log(state);
+const store = createStore(reducer);
+store.subscribe( () => {
+    console.log('ayyy',store.getState())
+});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'INC'});
+store.dispatch({type: 'DEC'});
