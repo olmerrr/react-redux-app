0ccaf7cea0c9902b22272cb2da684dce13c7294b
Yaroslav <dombrovskiy91@gmail.com>
2020-10-19 16:28:32 +0300

    subscribe+dispatch+clean JS + HTML

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
-import {createStore} from "redux";
+import {createStore} from  "redux";
 
-
-const reducer = (state = 0, action) => {
-    switch (action.type) {
+// const reducer = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INC':
+//             return state + 1;
+//         case 'DEC':
+//             return state - 1;
+//
+//         default:
+//             return state;
+//     }
+// };
+const reducer = (state = 0, action) =>{
+    switch (action.type){
         case 'INC':
             return state + 1;
         case 'DEC':
             return state - 1;
-
         default:
             return state;
     }
-    return 0;
 };
 const store = createStore(reducer);
-store.subscribe( () => {
-    console.log('ayyy',store.getState())
-});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'DEC'});
+document
+    .getElementById('inc')
+    .addEventListener('click', () =>{
+        store.dispatch({type: 'INC'})
+    });
+document
+    .getElementById('dec')
+    .addEventListener('click', () => {
+        store.dispatch({type: 'DEC'})
+    });
+
+const update = () => {
+    document
+        .getElementById('counter')
+        .innerHTML = store.getState();
+};
+store.subscribe(update);
