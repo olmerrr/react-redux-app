312357793f81aa0f44f4893ba0f67f2fbd4fafaf
Yaroslav <dombrovskiy91@gmail.com>
2020-10-20 15:13:24 +0300

    bindActionCreater() my vers

--- src/index.js
+++ src/index.js
@@ -7,23 +7,26 @@ import {
 } from "./actions";
 
 const store = createStore(reducer);
+const {dispatch} = store;
+const bindActionCreater = (creator, dispatch) => (...args) => {
+  dispatch(creator(...args));
+};
+
+const incDispatch = bindActionCreater(inc, dispatch);
+const decDispatch = bindActionCreater(dec, dispatch);
+const rndDispatch = bindActionCreater(rnd, dispatch);
 
 document
     .getElementById('inc')
-    .addEventListener('click', () => {
-        store.dispatch(inc())
-    });
+    .addEventListener('click', incDispatch);
 document
     .getElementById('dec')
-    .addEventListener('click', () => {
-        store.dispatch(dec())
-    });
+    .addEventListener('click', decDispatch);
 document
     .getElementById('rnd')
     .addEventListener('click', () => {
-
         const payload = Math.floor(Math.random() * 10);
-        store.dispatch(rnd(payload))
+        rndDispatch(payload);
     })
 
 
