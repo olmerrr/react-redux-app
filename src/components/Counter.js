5707a42180e914616e509be7a7fc62123179f2dc
Yaroslav <dombrovskiy91@gmail.com>
2020-10-22 13:35:59 +0300

    mapDispatchToProps()

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {bindActionCreators} from "redux";
+import * as actions from '../actions';
 const Counter = ({counter, inc, dec, rnd}) => {
     return (
         <div className="jumbotron p-4">
@@ -21,4 +23,15 @@ const mapStateToProps = (state) => {
         counter: state
     }
 };
-export default connect(mapStateToProps)(Counter);
+const mapDispatchToProps = (dispatch) => {
+    const {inc, dec, rnd} = bindActionCreators(actions,dispatch);
+    return {
+        inc,
+        dec,
+        rnd: () => {
+            const randomValue = Math.floor(Math.random() * 10);
+           rnd(randomValue);
+        }
+    }
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
